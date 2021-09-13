b0c6c354b4ab44b6924b8b4b8a80ab31c938b709
ArseniyX <jkl@usa.com>
2021-09-13 17:21:37 +0300

    add numbers to diaplay deltethem and reset

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
 onload = () => {
+  const addComma = (num) => {
+    let str = num.split(".");
+    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str[0].join["."];
+  };
+
+  const reset = () => {
+    screenText.innerHTML = "";
+    num = "";
+  };
+
   const keys = [
     "7",
     "8",
@@ -21,21 +32,48 @@ onload = () => {
   ];
 
   const keyBoard = document.querySelector(".keyBoard");
+  const screenText = document.querySelector(".screenText");
+  let num = "";
+
   keys.forEach((e) => {
     const key = document.createElement("button");
     const number = document.createElement("p");
     number.classList.add("keyNumber");
     number.innerHTML = e;
     key.classList.add("keys");
-    if (e === "DEL") key.classList.add("grayButton");
+    if (e === "DEL") {
+      key.classList.add("grayButton");
+      key.addEventListener("click", () => {
+        //   screenText.innerHTML = screenText.innerHTML.slice(0, -1);
+        num = num.slice(0, -1);
+        let str = num.split(".");
+        str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        screenText.innerHTML = str.join(".");
+      });
+    }
     if (e === "RESET") {
       key.classList.add("grayButton");
       key.classList.add("resetButton");
+      key.addEventListener("click", () => {
+        reset();
+      });
     }
     if (e === "=") {
       key.classList.add("grayButton");
       key.classList.add("equalsButton");
+      key.addEventListener("click", () => {
+        reset();
+      });
     }
+    key.addEventListener("click", () => {
+      if (!isNaN(parseInt(e)) && 14 > screenText.innerHTML.length) {
+        num += e;
+        screenText.append(e);
+        let str = num.split(".");
+        str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        screenText.innerHTML = str.join(".");
+      }
+    });
     key.appendChild(number);
     keyBoard.appendChild(key);
   });
